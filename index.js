8b3be72751d9fcfc3139c5a2213a352d63ab4555
Masum Billah <masumbillah2062003@gmail.com>
2025-10-26 21:01:24 +0600

    change

--- index.js
+++ index.js
@@ -59,13 +59,14 @@ async function run() {
       })
     }
 
-    // users verify admin after verifyToken
+    // users verify admin after verifyToken 
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
       const query = { email: email }
 
       const user = await usersCollection.findOne(query)
       const isAdmin = user?.role === 'admin';
+      
       if (!isAdmin) {
         return res.status(403).send({ message: "forbidden access" })
       }
